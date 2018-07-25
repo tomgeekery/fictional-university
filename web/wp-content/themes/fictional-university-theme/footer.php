12afa54dfb486fac6be033357e74fc1806207fc1
Chris Keller <ckeller@pop-os.localdomain>
2018-07-25 09:40:03 -0400

    Added a couple dynamic menus

--- web/wp-content/themes/fictional-university-theme/footer.php
+++ web/wp-content/themes/fictional-university-theme/footer.php
@@ -13,23 +13,22 @@
           <div class="site-footer__col-two">
             <h3 class="headline headline--small">Explore</h3>
             <nav class="nav-list">
-              <ul>
-                <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Campuses</a></li>
-              </ul>
+              <?php
+                wp_nav_menu(array(
+                  'theme_location' => 'footerLocationOne',
+                ));
+              ?>
             </nav>
           </div>
 
           <div class="site-footer__col-three">
             <h3 class="headline headline--small">Learn</h3>
             <nav class="nav-list">
-              <ul>
-                <li><a href="#">Legal</a></li>
-                <li><a href="<?php echo site_url('/privacy-policy'); ?>">Privacy</a></li>
-                <li><a href="#">Careers</a></li>
-              </ul>
+              <?php
+                wp_nav_menu(array(
+                  'theme_location' => 'footerLocationTwo',
+                ));
+              ?>
             </nav>
           </div>
         </div>
