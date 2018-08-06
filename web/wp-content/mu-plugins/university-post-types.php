1d7a634e663f90660371fabe53660ef9543d6ea5
Chris Keller <kellerch@umich.edu>
2018-08-06 08:20:27 -0400

    Misc updates

--- web/wp-content/mu-plugins/university-post-types.php
+++ web/wp-content/mu-plugins/university-post-types.php
@@ -3,6 +3,7 @@
 
 function university_post_types() {
 	register_post_type( 'event', array(
+		'supports'    => array( 'title', 'editor', 'excerpt' ),
 		'rewrite'     => array( 'slug' => 'events' ),
 		'has_archive' => true,
 		'public'      => true,
--- web/wp-content/themes/fictional-university-theme/front-page.php
+++ web/wp-content/themes/fictional-university-theme/front-page.php
@@ -37,7 +37,12 @@
                             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                         </h5>
 
-                        <p><?php echo wp_trim_words( get_the_content(), 18 ); ?>
+                        <p>
+							<?php if ( has_excerpt() ) {
+								echo get_the_excerpt();
+							} else {
+								echo wp_trim_words( get_the_content(), 18 );
+							} ?>
                             <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
                         </p>
                     </div>
@@ -46,7 +51,9 @@
 			<?php }
 			?>
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+            <p class="t-center no-margin">
+                <a href="<?php echo get_post_type_archive_link( 'event' ); ?>" class="btn btn--blue">View All Events</a>
+            </p>
 
         </div>
     </div>
@@ -68,11 +75,18 @@
                         <span class="event-summary__day"><?php the_time( 'd' ); ?></span>
                     </a>
                     <div class="event-summary__content">
-                        <h5 class="event-summary__title headline headline--tiny"><a
-                                    href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        <h5 class="event-summary__title headline headline--tiny">
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                         </h5>
-                        <p><?php echo wp_trim_words( get_the_content(), 18 ); ?> <a href="<?php the_permalink(); ?>"
-                                                                                    class="nu gray">Read more</a></p>
+
+                        <p>
+							<?php if ( has_excerpt() ) {
+								echo get_the_excerpt();
+							} else {
+								echo wp_trim_words( get_the_content(), 18 );
+							} ?>
+                            <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a>
+                        </p>
                     </div>
                 </div>
 			<?php }
