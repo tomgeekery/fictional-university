900120177ed49753801afc527942133fc378bec9
Chris Keller <kellerch@umich.edu>
2018-08-06 08:53:28 -0400

    Custom fields

--- web/wp-content/themes/fictional-university-theme/front-page.php
+++ web/wp-content/themes/fictional-university-theme/front-page.php
@@ -29,8 +29,16 @@
 
                 <div class="event-summary">
                     <a class="event-summary__date t-center" href="#">
-                        <span class="event-summary__month">Mar</span>
-                        <span class="event-summary__day">25</span>
+                        <span class="event-summary__month">
+                            <?php
+                            $eventDate = new DateTime( get_field( 'event_date' ) );
+                            echo $eventDate->format( 'M' );
+                            ?>
+                        </span>
+
+                        <span class="event-summary__day">
+                            <?php echo $eventDate->format( 'd' ); ?>
+                        </span>
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny">
