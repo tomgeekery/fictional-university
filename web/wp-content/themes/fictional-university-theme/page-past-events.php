ff757cc985b0d469397e47e474a1229de6db1b89
Chris Keller <kellerch@umich.edu>
2018-08-07 08:45:23 -0400

    Custom query pagination

--- web/wp-content/themes/fictional-university-theme/page-past-events.php
+++ web/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -4,17 +4,35 @@
     <div class="page-banner__bg-image"
          style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ); ?>);"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Events</h1>
+        <h1 class="page-banner__title">Past Events</h1>
         <div class="page-banner__intro">
-            <p>See what is going on in our world.</p>
+            <p>A recap of our past events.</p>
         </div>
     </div>
 </div>
 
 <div class="container container--narrow page-section">
 	<?php
-	while ( have_posts() ) {
-		the_post(); ?>
+	$today = date( 'Ymd' );
+
+	$pastEvents = new WP_Query( array(
+		'paged'      => get_query_var( 'paged', 1 ),
+		'post_type'  => 'event',
+		'orderby'    => 'meta_value_num',
+		'meta_key'   => 'event_date',
+		'order'      => 'ASC',
+		'meta_query' => array(
+			array(
+				'key'     => 'event_date',
+				'compare' => '<',
+				'value'   => $today,
+				'type'    => 'numeric',
+			)
+		),
+	) );
+
+	while ( $pastEvents->have_posts() ) {
+		$pastEvents->the_post(); ?>
 
         <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
@@ -34,7 +52,8 @@
                     <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                 </h5>
 
-                <p><?php echo wp_trim_words( get_the_content(), 18 ); ?>
+                <p>
+					<?php echo wp_trim_words( get_the_content(), 18 ); ?>
                     <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
                 </p>
             </div>
@@ -42,11 +61,9 @@
 
 	<?php } ?>
 
-	<?php echo paginate_links(); ?>
-
-    <hr class="section-break">
-
-    <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive</a>.</p>
+	<?php echo paginate_links( array(
+		'total' => $pastEvents->max_num_pages,
+	) ); ?>
 
 </div>
 
