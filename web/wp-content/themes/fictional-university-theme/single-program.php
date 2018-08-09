3ef12b63977bbbd9733e775280cc55c265ae3888
Chris Keller <kellerch@umich.edu>
2018-08-09 07:39:19 -0400

    Quick program edits

--- web/wp-content/themes/fictional-university-theme/single-program.php
+++ web/wp-content/themes/fictional-university-theme/single-program.php
@@ -45,20 +45,24 @@ while ( have_posts() ) {
 					'value'   => $today,
 					'type'    => 'numeric',
 				),
-                array(
-                    'key' => 'related_program',
-                    'compare' => 'LIKE',
-                    'value' => '"' .  get_the_ID() . '"',
-                ),
+				array(
+					'key'     => 'related_program',
+					'compare' => 'LIKE',
+					'value'   => '"' . get_the_ID() . '"',
+				),
 			),
 		) );
 
-		while ( $homepageEvents->have_posts() ) {
-			$homepageEvents->the_post();
-			?>
+		if ( $homepageEvents->have_posts() ) {
+			echo '<hr class="section-break">';
+			echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
 
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
+			while ( $homepageEvents->have_posts() ) {
+				$homepageEvents->the_post();
+				?>
+
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href="#">
                     <span class="event-summary__month">
                         <?php
                         $eventDate = new DateTime( get_field( 'event_date' ) );
@@ -66,27 +70,28 @@ while ( have_posts() ) {
                         ?>
                     </span>
 
-                    <span class="event-summary__day">
+                        <span class="event-summary__day">
                         <?php echo $eventDate->format( 'd' ); ?>
                     </span>
-                </a>
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny">
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                    </h5>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny">
+                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        </h5>
 
-                    <p>
-						<?php if ( has_excerpt() ) {
-							echo get_the_excerpt();
-						} else {
-							echo wp_trim_words( get_the_content(), 18 );
-						} ?>
-                        <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
-                    </p>
+                        <p>
+							<?php if ( has_excerpt() ) {
+								echo get_the_excerpt();
+							} else {
+								echo wp_trim_words( get_the_content(), 18 );
+							} ?>
+                            <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
+                        </p>
+                    </div>
                 </div>
-            </div>
 
-		<?php }
+			<?php }
+		}
 		?>
 
     </div>
