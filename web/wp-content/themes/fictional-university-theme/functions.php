2628f05ca418890b633882d108da1d7a66fcff32
Chris Keller <kellerch@umich.edu>
2018-08-09 09:26:36 -0400

    Reduce duplicate code

--- web/wp-content/themes/fictional-university-theme/functions.php
+++ web/wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,37 @@
 <?php
 
+function pageBanner( $args = array() ) {
+	if ( ! isset( $args['title'] ) ) {
+		$args['title'] = get_the_title();
+	}
+
+	if ( ! isset( $args['subtitle'] ) ) {
+		$args['subtitle'] = get_field( 'page_banner_subtitle' );
+	}
+
+	if ( ! isset( $args['photo'] ) ) {
+		if ( get_field( 'page_banner_background_image' ) ) {
+			$args['photo'] = get_field( 'page_banner_background_image' )['sizes']['pageBanner'];
+		} else {
+			$args['photo'] = get_theme_file_uri( '/images/ocean.jpg' );
+		}
+	}
+	?>
+
+    <div class="page-banner">
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo $args['photo'] ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+            <div class="page-banner__intro">
+                <p><?php echo $args['subtitle'] ?></p>
+            </div>
+        </div>
+    </div>
+
+	<?php
+}
+
 function university_files() {
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
@@ -16,9 +48,9 @@ function university_features() {
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
 
-	add_image_size('professorLandscape', 400, 260, true);
-	add_image_size('professorPortrait', 480, 650, true);
-	add_image_size('pageBanner', 1500, 350, true);
+	add_image_size( 'professorLandscape', 400, 260, true );
+	add_image_size( 'professorPortrait', 480, 650, true );
+	add_image_size( 'pageBanner', 1500, 350, true );
 }
 
 function university_adjust_queries( WP_Query $query ) {
--- web/wp-content/themes/fictional-university-theme/single-professor.php
+++ web/wp-content/themes/fictional-university-theme/single-professor.php
@@ -3,34 +3,23 @@
 get_header();
 
 while ( have_posts() ) {
-	the_post(); ?>
+	the_post();
+	pageBanner();
+	?>
 
-	<div class="page-banner">
-        <?php $pageBannerImage = get_field('page_banner_background_image'); ?>
+    <div class="container container--narrow page-section">
 
-		<div class="page-banner__bg-image"
-		     style="background-image: url(<?php echo $pageBannerImage['sizes']['pageBanner']; ?>);"></div>
-		<div class="page-banner__content container container--narrow">
-			<h1 class="page-banner__title"><?php the_title() ?></h1>
-			<div class="page-banner__intro">
-				<p><?php the_field('page_banner_subtitle'); ?></p>
-			</div>
-		</div>
-	</div>
+        <div class="generic-content">
+            <div class="row group">
+                <div class="one-third">
+					<?php the_post_thumbnail( 'professorPortrait' ); ?>
+                </div>
 
-	<div class="container container--narrow page-section">
-
-		<div class="generic-content">
-			<div class="row group">
-				<div class="one-third">
-					<?php the_post_thumbnail('professorPortrait'); ?>
-				</div>
-
-				<div class="two-thirds">
+                <div class="two-thirds">
 					<?php the_content(); ?>
-				</div>
-			</div>
-		</div>
+                </div>
+            </div>
+        </div>
 
 		<?php
 
@@ -41,15 +30,15 @@ while ( have_posts() ) {
 			echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
 			echo '<ul class="link-list min-list">';
 			foreach ( $relatedPrograms as $program ) { ?>
-				<li><a href="<?php echo get_the_permalink( $program ) ?>"><?php echo get_the_title( $program ); ?></a>
-				</li>
+                <li><a href="<?php echo get_the_permalink( $program ) ?>"><?php echo get_the_title( $program ); ?></a>
+                </li>
 			<?php }
 			echo '</ul>';
 		}
 
 		?>
 
-	</div>
+    </div>
 
 <?php }
 
