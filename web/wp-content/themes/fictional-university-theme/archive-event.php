64231762476edd3a75f7824313428abc24092573
Chris Keller <kellerch@umich.edu>
2018-08-10 08:16:16 -0400

    Using pagebanner function

--- web/wp-content/themes/fictional-university-theme/archive-event.php
+++ web/wp-content/themes/fictional-university-theme/archive-event.php
@@ -1,15 +1,8 @@
 <?php get_header(); ?>
-
-<div class="page-banner">
-    <div class="page-banner__bg-image"
-         style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ); ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All Events</h1>
-        <div class="page-banner__intro">
-            <p>See what is going on in our world.</p>
-        </div>
-    </div>
-</div>
+<?php pageBanner( array(
+	'title'    => 'All Events',
+	'subtitle' => 'See what is going on in our world.',
+) ); ?>
 
 <div class="container container--narrow page-section">
 	<?php
@@ -46,7 +39,8 @@
 
     <hr class="section-break">
 
-    <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive</a>.</p>
+    <p>Looking for a recap of past events? <a href="<?php echo site_url( '/past-events' ); ?>">Check out our past events
+            archive</a>.</p>
 
 </div>
 
--- web/wp-content/themes/fictional-university-theme/page-past-events.php
+++ web/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -1,15 +1,9 @@
 <?php get_header(); ?>
 
-<div class="page-banner">
-    <div class="page-banner__bg-image"
-         style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ); ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">Past Events</h1>
-        <div class="page-banner__intro">
-            <p>A recap of our past events.</p>
-        </div>
-    </div>
-</div>
+<?php pageBanner( array(
+	'title'    => 'Past Events',
+	'subtitle' => 'A recap of our past events.',
+) ); ?>
 
 <div class="container container--narrow page-section">
 	<?php
