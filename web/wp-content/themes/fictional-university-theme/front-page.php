4f78e4cc4cff31763669087b9abb4d9148bd898d
Chris Keller <kellerch@umich.edu>
2018-08-10 08:28:42 -0400

    Reduce duplication with get_template_part

--- web/wp-content/themes/fictional-university-theme/front-page.php
+++ web/wp-content/themes/fictional-university-theme/front-page.php
@@ -8,7 +8,8 @@
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re
             interested in?</h3>
-        <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
+        <a href="<?php echo get_post_type_archive_link( 'program' ); ?>" class="btn btn--large btn--blue">Find Your
+            Major</a>
     </div>
 </div>
 
@@ -38,42 +39,14 @@
 
 			while ( $homepageEvents->have_posts() ) {
 				$homepageEvents->the_post();
-				?>
-
-                <div class="event-summary">
-                    <a class="event-summary__date t-center" href="#">
-                        <span class="event-summary__month">
-                            <?php
-                            $eventDate = new DateTime( get_field( 'event_date' ) );
-                            echo $eventDate->format( 'M' );
-                            ?>
-                        </span>
-
-                        <span class="event-summary__day">
-                            <?php echo $eventDate->format( 'd' ); ?>
-                        </span>
-                    </a>
-                    <div class="event-summary__content">
-                        <h5 class="event-summary__title headline headline--tiny">
-                            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                        </h5>
-
-                        <p>
-							<?php if ( has_excerpt() ) {
-								echo get_the_excerpt();
-							} else {
-								echo wp_trim_words( get_the_content(), 18 );
-							} ?>
-                            <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
-                        </p>
-                    </div>
-                </div>
-
-			<?php }
+				get_template_part( 'template-parts/content', 'event' );
+			}
 			?>
 
             <p class="t-center no-margin">
-                <a href="<?php echo get_post_type_archive_link( 'event' ); ?>" class="btn btn--blue">View All Events</a>
+                <a href="<?php echo get_post_type_archive_link( 'event' ); ?>" class="btn btn--blue">
+                    View All Events
+                </a>
             </p>
 
         </div>
