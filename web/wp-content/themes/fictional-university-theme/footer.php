51f522b93ea5d4d49e0095d03f389e4cb01ce491
Chris Keller <kellerch@umich.edu>
2018-08-14 08:46:03 -0400

    Open and close search overlay

--- web/wp-content/themes/fictional-university-theme/footer.php
+++ web/wp-content/themes/fictional-university-theme/footer.php
@@ -2,53 +2,69 @@
 
     <div class="site-footer__inner container container--narrow">
 
-      <div class="group">
+        <div class="group">
 
-        <div class="site-footer__col-one">
-          <h1 class="school-logo-text school-logo-text--alt-color"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
-          <p><a class="site-footer__link" href="#">555.555.5555</a></p>
+            <div class="site-footer__col-one">
+                <h1 class="school-logo-text school-logo-text--alt-color"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong>
+                        University</a></h1>
+                <p><a class="site-footer__link" href="#">555.555.5555</a></p>
+            </div>
+
+            <div class="site-footer__col-two-three-group">
+                <div class="site-footer__col-two">
+                    <h3 class="headline headline--small">Explore</h3>
+                    <nav class="nav-list">
+						<?php
+						wp_nav_menu( array(
+							'theme_location' => 'footerLocationOne',
+						) );
+						?>
+                    </nav>
+                </div>
+
+                <div class="site-footer__col-three">
+                    <h3 class="headline headline--small">Learn</h3>
+                    <nav class="nav-list">
+						<?php
+						wp_nav_menu( array(
+							'theme_location' => 'footerLocationTwo',
+						) );
+						?>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="site-footer__col-four">
+                <h3 class="headline headline--small">Connect With Us</h3>
+                <nav>
+                    <ul class="min-list social-icons-list group">
+                        <li><a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        </li>
+                        <li><a href="#" class="social-color-twitter"><i class="fa fa-twitter"
+                                                                        aria-hidden="true"></i></a></li>
+                        <li><a href="#" class="social-color-youtube"><i class="fa fa-youtube"
+                                                                        aria-hidden="true"></i></a></li>
+                        <li><a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </li>
+                        <li><a href="#" class="social-color-instagram"><i class="fa fa-instagram"
+                                                                          aria-hidden="true"></i></a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
-
-        <div class="site-footer__col-two-three-group">
-          <div class="site-footer__col-two">
-            <h3 class="headline headline--small">Explore</h3>
-            <nav class="nav-list">
-              <?php
-                wp_nav_menu(array(
-                  'theme_location' => 'footerLocationOne',
-                ));
-              ?>
-            </nav>
-          </div>
-
-          <div class="site-footer__col-three">
-            <h3 class="headline headline--small">Learn</h3>
-            <nav class="nav-list">
-              <?php
-                wp_nav_menu(array(
-                  'theme_location' => 'footerLocationTwo',
-                ));
-              ?>
-            </nav>
-          </div>
-        </div>
-
-        <div class="site-footer__col-four">
-          <h3 class="headline headline--small">Connect With Us</h3>
-          <nav>
-            <ul class="min-list social-icons-list group">
-              <li><a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-              <li><a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
 
     </div>
-  </footer>
+</footer>
+
+<div class="search-overlay">
+    <div class="search-overlay__top">
+        <div class="container">
+            <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+        </div>
+    </div>
+</div>
 
 <?php wp_footer(); ?>
 
--- web/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ web/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -1,7 +1,29 @@
+import $ from 'jquery';
+
 class Search {
+
     constructor() {
-        alert("Search");
+        this.openButton = $(".js-search-trigger");
+        this.closeButton = $(".search-overlay__close");
+        this.searchOverlay = $(".search-overlay");
+        this.events();
     }
+
+    // Events
+    events() {
+        this.openButton.on("click", this.openOverlay.bind(this));
+        this.closeButton.on("click", this.closeOverlay.bind(this));
+    }
+
+    // Methods
+    openOverlay() {
+        this.searchOverlay.addClass("search-overlay--active");
+    }
+
+    closeOverlay() {
+        this.searchOverlay.removeClass("search-overlay--active");
+    }
+
 }
 
 export default Search;
