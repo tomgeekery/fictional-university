34a4c043f0edf432b21f4a3fa7d91a465be7880a
Chris Keller <kellerch@umich.edu>
2018-08-15 08:32:50 -0400

    Managing time in javascript

--- web/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ web/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -2,12 +2,15 @@ import $ from 'jquery';
 
 class Search {
 
+    // Constructor
     constructor() {
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+        this.searchField = $("#search-term");
 
         this.isOverlayOpen = false;
+        this.typingTimer;
 
         this.events();
     }
@@ -17,10 +20,19 @@ class Search {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
 
+        this.searchField.on("keydown", this.typingLogic.bind(this));
         $(document).on("keydown", this.keyPressDispatcher.bind(this));
     }
 
     // Methods
+    typingLogic() {
+        clearTimeout(this.typingTimer);
+
+        this.typingTimer = setTimeout(function() {
+            console.log("howdy");
+        }, 2000);
+    }
+
     keyPressDispatcher(event) {
         if (event.keyCode === 83 && !this.isOverlayOpen) {
             this.openOverlay();
