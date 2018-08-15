45ddce2159ca747348ed732616a3cbb8808fb60e
Chris Keller <kellerch@umich.edu>
2018-08-15 08:16:57 -0400

    Keyboard events in javascript

--- web/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ web/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -6,6 +6,9 @@ class Search {
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+
+        this.isOverlayOpen = false;
+
         this.events();
     }
 
@@ -13,15 +16,31 @@ class Search {
     events() {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
+
+        $(document).on("keydown", this.keyPressDispatcher.bind(this));
     }
 
     // Methods
+    keyPressDispatcher(event) {
+        if (event.keyCode === 83 && !this.isOverlayOpen) {
+            this.openOverlay();
+        }
+
+        if (event.keyCode === 27 && this.isOverlayOpen) {
+            this.closeOverlay();
+        }
+    }
+
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
+        $("body").addClass("body-no-scroll");
+        this.isOverlayOpen = true;
     }
 
     closeOverlay() {
         this.searchOverlay.removeClass("search-overlay--active");
+        $("body").removeClass("body-no-scroll");
+        this.isOverlayOpen = false;
     }
 
 }
