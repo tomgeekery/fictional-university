9f5e03040ff3721f47257d574bee84496c8ee21b
Chris Keller <kellerch@umich.edu>
2018-08-16 08:56:40 -0400

    Quick misc edits

--- web/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ web/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -4,6 +4,8 @@ class Search {
 
     // Constructor
     constructor() {
+        this.addSearchHTML();
+
         this.resultsDiv = $("#search-overlay__results");
 
         this.openButton = $(".js-search-trigger");
@@ -40,7 +42,7 @@ class Search {
                     this.isSpinnerVisible = true;
                 }
 
-                this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+                this.typingTimer = setTimeout(this.getResults.bind(this), 750);
             }
 
             else {
@@ -78,6 +80,11 @@ class Search {
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
         $("body").addClass("body-no-scroll");
+
+        this.searchField.val("");
+        this.resultsDiv.html("");
+        setTimeout(() => this.searchField.focus(), 301);
+
         this.isOverlayOpen = true;
     }
 
@@ -85,6 +92,24 @@ class Search {
         this.searchOverlay.removeClass("search-overlay--active");
         $("body").removeClass("body-no-scroll");
         this.isOverlayOpen = false;
+    }
+
+    addSearchHTML() {
+        $("body").append(`
+            <div class="search-overlay">
+                <div class="search-overlay__top">
+                    <div class="container">
+                        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                        <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+                        <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+                    </div>
+                </div>
+            
+                <div class="container">
+                    <div id="search-overlay__results"></div>
+                </div>
+            </div>
+        `);
     }
 
 }
