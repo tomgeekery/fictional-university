fe2f8eaa9cdf3eb3c8ec02aeede1e1b9770e1357
Chris Keller <kellerch@umich.edu>
2018-08-16 08:25:40 -0400

    Generate HTML based on json

--- web/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ web/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -53,8 +53,13 @@ class Search {
     }
 
     getResults() {
-        $.getJSON("http://localhost:3000/wp-json/wp/v2/posts?search=" + this.searchField.val(), function(posts) {
-            console.log(posts[0].title.rendered);
+        $.getJSON("http://localhost:3000/wp-json/wp/v2/posts?search=" + this.searchField.val(), posts => {
+            this.resultsDiv.html(`
+                <h2 class="search-overlay__section-title">General Information</h2>
+                <ul class="link-list min-list">
+                    ${ posts.map(item => `<li><a href="${ item.link }">${ item.title.rendered }</a></li>`).join('') }
+                </ul>
+            `);
         });
     }
 
