40559a9beb097831c2fc3d9e2e689fefd070291a
Chris Keller <kellerch@umich.edu>
2018-08-17 08:26:54 -0400

    Synchronous vs asynchronous #1

--- web/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ web/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -56,14 +56,18 @@ class Search {
 
     getResults() {
         $.getJSON(universityData.root_url + "/wp-json/wp/v2/posts?search=" + this.searchField.val(), posts => {
-            this.resultsDiv.html(`
-                <h2 class="search-overlay__section-title">General Information</h2>
-                ${ posts.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>' }
-                    ${ posts.map(item => `<li><a href="${ item.link }">${ item.title.rendered }</a></li>`).join('') }
-                ${ posts.length ? '</ul>' : '' }
-            `);
+            $.getJSON(universityData.root_url + "/wp-json/wp/v2/pages?search=" + this.searchField.val(), pages => {
+                var combinedResults = posts.concat(pages);
 
-            this.isSpinnerVisible = false;
+                this.resultsDiv.html(`
+                    <h2 class="search-overlay__section-title">General Information</h2>
+                    ${ combinedResults.length ? '<ul class="link-list min-list">' : '<p>No general information matches that search.</p>' }
+                        ${ combinedResults.map(item => `<li><a href="${ item.link }">${ item.title.rendered }</a></li>`).join('') }
+                    ${ combinedResults.length ? '</ul>' : '' }
+                `);
+
+                this.isSpinnerVisible = false;
+            })
         });
     }
 
