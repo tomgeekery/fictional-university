98e7cb9ff8b607cde6cb747f8a6911510067953a
Chris Keller <kellerch@umich.edu>
2018-08-20 08:11:30 -0400

    WP_Query and keyword searching

--- web/wp-content/themes/fictional-university-theme/inc/search-route.php
+++ web/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -1,29 +1,30 @@
 <?php
 
 function university_register_search() {
-	register_rest_route('university/v1', 'search', array(
-		'methods' => WP_REST_SERVER::READABLE,
+	register_rest_route( 'university/v1', 'search', array(
+		'methods'  => WP_REST_SERVER::READABLE,
 		'callback' => 'university_search_results',
-	));
+	) );
 }
 
-function university_search_results() {
-	$professors = new WP_Query(array(
+function university_search_results( $data ) {
+	$professors = new WP_Query( array(
 		'post_type' => 'professor',
-	));
+		's'         => sanitize_text_field( $data['term'] ),
+	) );
 
 	$professor_results = array();
 
-	while ($professors->have_posts()) {
+	while ( $professors->have_posts() ) {
 		$professors->the_post();
 
-		array_push($professor_results, array(
-			'title' => get_the_title(),
+		array_push( $professor_results, array(
+			'title'     => get_the_title(),
 			'permalink' => get_the_permalink(),
-		));
+		) );
 	}
 
 	return $professor_results;
 }
 
-add_action('rest_api_init', 'university_register_search');
+add_action( 'rest_api_init', 'university_register_search' );
