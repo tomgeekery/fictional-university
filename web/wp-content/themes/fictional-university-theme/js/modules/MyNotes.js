d70bba54a6fbb4609245cd912cd3fa92f5088fc3
Chris Keller <kellerch@umich.edu>
2018-08-27 08:53:45 -0400

    Edit / update posts with the rest api

--- web/wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ web/wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -7,15 +7,58 @@ class MyNotes {
 
     events() {
         $(".delete-note").on("click", this.deleteNote);
-        $(".edit-note").on("click", this.editNote);
+        $(".edit-note").on("click", this.editNote.bind(this));
+        $(".update-note").on("click", this.updateNote.bind(this));
     }
 
     // Methods
+    updateNote(event) {
+        var thisNote = $(event.target).parents("li");
+
+        var ourUpdatedPost = {
+            'title': thisNote.find(".note-title-field").val(),
+            'content': thisNote.find(".note-body-field").val(),
+        }
+
+        $.ajax({
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+            },
+            url: universityData.root_url + "/wp-json/wp/v2/note/" + thisNote.data("id"),
+            type: "POST",
+            data: ourUpdatedPost,
+            success: (response) => {
+                this.makeNoteReadOnly(thisNote);
+            },
+            error: (response) => {
+                console.log("Sorry");
+                console.log(response);
+            },
+        });
+    }
+
     editNote(event) {
         var thisNote = $(event.target).parents("li");
 
+        if (thisNote.data("state") == "editable") {
+            this.makeNoteReadOnly(thisNote);
+        } else {
+            this.makeNoteEditable(thisNote);
+        }
+    }
+
+    makeNoteEditable(thisNote) {
+        thisNote.find(".edit-note").html('<i class="fa fa-times" aria-hidden="true"></i> Cancel');
         thisNote.find(".note-title-field, .note-body-field").removeAttr("readonly").addClass("note-active-field");
         thisNote.find(".update-note").addClass("update-note--visible");
+        thisNote.data("state", "editable");
+    }
+
+    makeNoteReadOnly(thisNote) {
+        thisNote.find(".edit-note").html('<i class="fa fa-pencil" aria-hidden="true"></i> Edit');
+        thisNote.find(".note-title-field, .note-body-field").attr("readonly", "readonly").removeClass("note-active-field");
+        thisNote.find(".update-note").removeClass("update-note--visible");
+        thisNote.data("state", "readonly");
     }
 
     deleteNote(event) {
--- web/wp-content/themes/fictional-university-theme/page-my-notes.php
+++ web/wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -26,10 +26,10 @@ while ( have_posts() ) {
 
 			    <li data-id="<?php the_ID(); ?>">
                     <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
-                    <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
-                    <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
+                    <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
+                    <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
                     <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
-                    <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
+                    <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
                 </li>
 			<?php }
 			?>
