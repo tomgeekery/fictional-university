c468149dbc79a30f9d266d3be66925e54ca1a6de
Chris Keller <kellerch@umich.edu>
2018-08-30 08:01:01 -0400

    Creating new notes part 2

--- web/wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
+++ web/wp-content/themes/fictional-university-theme/js/modules/MyNotes.js
@@ -6,9 +6,9 @@ class MyNotes {
     }
 
     events() {
-        $(".delete-note").on("click", this.deleteNote);
-        $(".edit-note").on("click", this.editNote.bind(this));
-        $(".update-note").on("click", this.updateNote.bind(this));
+        $("#my-notes").on("click", ".delete-note", this.deleteNote);
+        $("#my-notes").on("click", ".edit-note", this.editNote.bind(this));
+        $("#my-notes").on("click", ".update-note", this.updateNote.bind(this));
         $(".submit-note").on("click", this.createNote.bind(this));
     }
 
@@ -29,7 +29,16 @@ class MyNotes {
             data: ourNewPost,
             success: (response) => {
                 $(".new-note-title, .new-note-body").val("");
-                $('<li>Imageine real data here</li>').prependTo("#my-notes").hide().slideDown();
+
+                $(`
+                    <li data-id="${response.id}">
+                        <input readonly class="note-title-field" value="${response.title.raw}">
+                        <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
+                        <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
+                        <textarea readonly class="note-body-field">${response.content.raw}</textarea>
+                        <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
+                    </li>
+                `).prependTo("#my-notes").hide().slideDown();
             },
             error: (response) => {
                 console.log("Sorry, create new post didn't work!!");
